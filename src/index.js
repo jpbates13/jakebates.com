e243f3e263096727ac45ee391cd1943fdc31e09a
jpbates13 <jpbates13@gmail.com>
2022-04-24 19:01:37 -0400

    This fix won't work for dynamic pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Helmet } from "react-helmet";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Helmet>
-      <title>JakeBates.com</title>
-    </Helmet>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
