33ba106902294b071c1836f9c9a8097a3ab80e71
Jake Bates <jpbates13@gmail.com>
2024-04-07 15:48:36 -0400

    added urls to tech stack logos

--- src/components/ProjectSlider/TechStack.js
+++ src/components/ProjectSlider/TechStack.js
@@ -18,20 +18,20 @@ import { Tooltip } from "@mui/material";
 
 function TechStack(props) {
   const techStackMapping = {
-    django: django,
-    firebase: firebase,
-    flask: flask,
-    java: java,
-    javascript: javascript,
-    postgres: postgres,
-    python: python,
-    react: react,
-    springboot: springboot,
-    html: html,
-    css: css,
-    github_pages: github,
-    android: android,
-    raspberry_pi: raspberry_pi
+    django: [django, "https://www.djangoproject.com/"],
+    firebase: [firebase, "https://firebase.google.com/"],
+    flask:[flask, "https://flask.palletsprojects.com/en/3.0.x/"],
+    java: [java, "https://www.java.com/"],
+    javascript: [javascript, "https://developer.mozilla.org/en-US/docs/Web/JavaScript"],
+    postgres: [postgres, "https://www.postgresql.org/"],
+    python: [python, "https://www.python.org/"],
+    react: [react, "https://reactjs.org/"],
+    springboot: [springboot, "https://spring.io/projects/spring-boot"],
+    html: [html, "https://developer.mozilla.org/en-US/docs/Web/HTML"],
+    css: [css, "https://developer.mozilla.org/en-US/docs/Web/CSS"],
+    github_pages: [github, "https://pages.github.com/"],
+    android: [android, "https://developer.android.com/"],
+    raspberry_pi: [raspberry_pi, "https://www.raspberrypi.org/"]
   };
 
   const techStack = props.techStack.split(",");
@@ -42,11 +42,12 @@ function TechStack(props) {
         return (
           techStackMapping[tech] && 
           <Tooltip title={tech} arrow>
+            <a href={techStackMapping[tech.toString().toLowerCase()][1]} target="_blank">
             <img
-              src={techStackMapping[tech.toString().toLowerCase()]}
-              style={{ width: "45px", height: "45px" }}
+              src={techStackMapping[tech.toString().toLowerCase()][0]}
+              style={{ width: "45px", height: "45px", cursor: "pointer"}}
               alt={tech}
-            />{" "}
+            /></a>{" "}
           </Tooltip>
 
         );
